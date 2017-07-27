06c8d69dfdced3bfd33c3de82654edd1ab5df0ed
Jan Bölsche <regular.gonzales@googlemail.com>
2017-07-27 18:16:48 +0200

    Don't lint if there's no text

--- json-editor/cm-jsonlint.js
+++ json-editor/cm-jsonlint.js
@@ -7,6 +7,7 @@ const jsonlint = require('./jsonlint')
 module.exports = function(CodeMirror) {
   CodeMirror.registerHelper("lint", "json", function(text) {
     var found = [];
+    if (text.length === 0) return found
     jsonlint.parser.parseError = function(str, hash) {
       var loc = hash.loc;
       found.push({from: CodeMirror.Pos(loc.first_line - 1, loc.first_column),
