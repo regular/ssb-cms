b2efa203992b6681c8c6140000458087a6d17545
Jan Bölsche <regular.gonzales@googlemail.com>
2017-08-31 16:47:22 +0200

    revisions() bugfix: make sure no old value is emitted after sync

--- db.js
+++ db.js
@@ -54,17 +54,17 @@ function filterRevisions() {
 module.exports = function(ssb, drafts) {
 
   function getMessageOrDraft(id, cb) {
-    let get
-    if (isDraft(id)) get = drafts.get
-    else if (ref.isMsg(id)) get = ssb.get
-    else throw new Error(`getMessageOrDraft: invalid id: ${id}`)
-    get(id, cb)
+    console.log('getting', id)
+    if (isDraft(id)) drafts.get(id, cb)
+    else if (ref.isMsg(id)) ssb.get(id, cb)
+    else cb(Error(`getMessageOrDraft: invalid id: ${id}`))
   }
 
   function filterSync(sync) {
     return pull.filter( x => {
       // only pass through the last expected sync
       if (sync && x.sync) {
+        console.log('sync, expect more:', sync-1)
         return (!--sync)
       }
       return true
@@ -110,13 +110,19 @@ module.exports = function(ssb, drafts) {
   function revisions(root, opts) {
     if (!root) throw new Error('Missing argument: root')
     opts = opts || {}
-    let syncCount = opts.sync ? (ref.isMsg(root) ? 2 : 1) : 0
+    let syncCount = opts.sync ? (ref.isMsg(root) ? 3 : 2) : 1
     return pull(
       many([
         pull(
           pull.once(root),
           pull.asyncMap(getMessageOrDraft),
-          pull.map( value => {return {key: root, value}})
+          pull.map( value => opts.sync ? [
+            { key: root, value},
+            { sync: true }
+          ] : [ 
+            { key: root, value}
+          ]),
+          pull.flatten()
         ),
         ref.isMsg(root) ? pull(
           ssb.links(Object.assign({}, opts, {
