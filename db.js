8fe9b3fc6861fad50cfd3725728e966dcc47a86b
Jan Bölsche <regular.gonzales@googlemail.com>
2017-08-31 21:05:21 +0200

    Implement getLatesst() in terms of revisions() and updatesStream()

--- db.js
+++ db.js
@@ -3,53 +3,9 @@ const pull = require('pull-stream')
 const many = require('pull-many')
 const ssbSort = require('ssb-sort')
 const deepAssign = require('deep-assign')
+const updatesStream = require('./update-stream')
 
 const {isDraft} = require('./util')
-
-// TODO: replace by update-stream
-function filterRevisions() {
-  // return only latest revisions
-  return function (read) {
-    var queue, _err, _cb
-    var heads = {}, roots = {}
-    var drain = pull.drain(function (msg) {
-      let c = msg.value.content
-      var revisionBranch = msg.value.revisionBranch || (c && c.revisionBranch)
-      if (revisionBranch) {
-        if (heads[revisionBranch]) delete heads[revisionBranch]
-        else roots[revisionBranch] = true
-      }
-      if (roots[msg.key]) delete roots[msg.key]
-      else heads[msg.key] = msg
-    }, function (_err) {
-      //if (err) throw err
-      queue = ssbSort(Object.keys(heads).map(key => heads[key]))
-      if (_cb) {
-        let cb = _cb
-        _cb = null
-        if (_err) cb(_err)
-        else if (queue.length) cb(null, queue.shift())
-        else cb(true)
-      } else {
-        _err = err
-      }
-    })(read)
-    return function (abort, cb) {
-      if (abort) {
-        if (drain) {
-          let _drain = drain
-          drain = null
-          return _drain.abort(abort, cb)
-        }
-        return read(abort, cb)
-      }
-      if (_err) cb(_err)
-      else if (!queue) _cb = cb
-      else if (queue.length) return cb(null, queue.shift())
-      else cb(true)
-    }
-  }
-}
 
 module.exports = function(ssb, drafts) {
 
@@ -139,45 +95,25 @@ module.exports = function(ssb, drafts) {
     )
   }
 
-  /// -- TODO: refactor below this line
-  
   // get latest revision of given revisionRoot
   // (including drafts)
   function getLatest(key, cb) {
     if (!key) return cb(new Error('no key specified'))
     if (isDraft(key)) return drafts.get(key, cb)
     pull(
-      many([
-        pull(
-          pull.once(key),
-          pull.asyncMap(ssb.get),
-          pull.map( x=>{return {key, value: x}})
-        ),
-        ssb.links({
-          rel: 'revisionRoot',
-          dest: key,
-          keys: true,
-          values: true
-        }),
-        drafts.byRevisionRoot(key)
-      ]),
-      filterRevisions(),
+      revisions(key),
+      updatesStream({bufferUntilSync: true}),
       pull.collect( (err, results)=>{
         if (err) return cb(err)
+        console.log('GET LATEST', results)
         if (results.length !== 1) return cb(new Error('got more or less than one result'))
         let msg = results[0].value
-        if (msg.msgString) {
-          try{
-            msg = JSON.parse(msg.msgString)
-          } catch(e) {
-            e.msgString = msg.msgString
-            return cb(e)
-          }
-        }
         cb(null, msg)
       })
     )
   }
+
+  /// -- TODO: refactor below this line
 
   function getPrototypeChain(key, result, cb) {
     getLatest(key, (err, msg)=>{
